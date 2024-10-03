7597e00db28cd2eb250ebf298b6521661df8d5f5
NguyennMinhh <minhnguyen8762382004@gmail.com>
2024-10-03 07:29:42 +0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,11 +1,29 @@
-import logo from './logo.svg';
 import './App.css';
+import Navbar from './Navbar';
 
 function App() {
+  // npm start to start rendering project
+  const title = 'Welcome';
+  const likes = 50;
+  // number, strings and array are fine while object and boolean, we can't!
+  const link = "https://www.google.com/";
+
   return (
-    <>
-      <h1>Hello</h1>
-    </>
+    <div className="App">
+      <Navbar />
+      <div className="content">
+
+        <h1>{ title } times likes</h1>
+        <p>Liked { likes } times</p>
+        <p>{ 10 * likes }</p>
+        <p>{ Math.random() * 10 }</p>
+
+        
+        {/* <a href="#">This is a link</a> */}
+        <a href={link}>Google</a>
+      </div>
+    </div>
+
   );
 }
 
